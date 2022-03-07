89bcce470e5230ac37fab701fc8b38ad084c6e0c
BuildTools <noahyoda1@gmail.com>
2022-03-06 20:55:03 -0700

    added missing functionality

--- autostop/autostop.js
+++ autostop/autostop.js
@@ -1,7 +1,11 @@
 function main(){
     let el = document.getElementsByClassName("ytp-autonav-toggle-button")[0];
+    let el2 = document.getElementsByClassName("ytp-button")[0];
+
     if(el.getAttribute("aria-checked")){
         el.setAttribute("aria-checked", false);
+        el2.setAttribute("aria-label", "Autoplay is off");
+        el22.setAttribute("title", "Autoplay is off");
         setTimeout(main, 300);
     }
 }
