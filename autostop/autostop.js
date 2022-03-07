ae9dac19e44f3537ea7cb6b78e73b6b45cd50689
BuildTools <noahyoda1@gmail.com>
2022-03-06 21:12:47 -0700

    fixed inf loop by changing true value expectation to string rather than bool

--- autostop/autostop.js
+++ autostop/autostop.js
@@ -1,12 +1,9 @@
 function main(){
     let el = document.getElementsByClassName("ytp-autonav-toggle-button")[0];
-    let el2 = document.getElementsByClassName("ytp-button")[0];
 
-    if(el.getAttribute("aria-checked")){
-        el.setAttribute("aria-checked", false);
-        el2.setAttribute("aria-label", "Autoplay is off");
-        el22.setAttribute("title", "Autoplay is off");
-        setTimeout(main, 300);
+    if(el.getAttribute("aria-checked") == "true"){
+        el.parentNode.parentNode.click();
+        setTimeout(main, 2000);
     }
 }
 
